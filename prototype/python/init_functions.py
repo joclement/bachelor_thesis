14b145b0509c3f8922d6ffff99aa253ea5f1edad
Joris Clement <7713214+joclement@users.noreply.github.com>
2016-05-18 18:49:43 +0200

    to implement different initilization and mate functions
    just a TODO commit

---
+++ prototype/python/init_functions.py
@@ -0,0 +1,9 @@
+#This file will contain different initilization functions
+
+#TODO create init functions for specific, own data
+
+#TODO create init function with fixed number of nodes
+
+#TODO create init function with flexible likelihood to get a node
+
+
---
+++ prototype/python/mate_functions.py
@@ -0,0 +1,3 @@
+#This file will contain different mate functions
+
+#TODO add mate function from Lochert2008c, which keeps the number of nodes equal
