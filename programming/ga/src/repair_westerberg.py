5739b8a6087cde9e84ee31f55ac2e8be57b3e1a9
Joris Clement <7713214+joclement@users.noreply.github.com>
2016-06-29 10:49:42 +0200

    test to repair westerberg data file, #14
    does not work, but it might be useful later, if I know how to do it.

---
+++ programming/ga/src/repair_westerberg.py
@@ -0,0 +1,44 @@
+import sys
+import io
+import numpy as np
+import constants
+
+import my_util
+
+import ralans_helper
+
+def repair():
+    """TODO: Docstring for repair.
+
+    :arg1: TODO
+    :returns: TODO
+
+    """
+    folder = "/home/joris/workspace/RaLaNS_data/westerberg_full/"
+    filename = "westerberg_full_try.txt"
+    filepath = folder + filename
+    newfilename = "westerberg_full_new.txt"
+    newfilepath = folder + newfilename
+    i = 0
+    with open(filepath, "r") as input:
+        with open(newfilepath,"wt") as output: 
+            for l in input:
+                line = ralans_helper.conv_byte_to_str(l)
+                try:
+                    linelist = np.loadtxt(io.StringIO(line)
+                            , delimiter=" ").tolist()
+                except Exception as e:
+                    print("error in line: ",i)
+                    print(l)
+                    sys.exit()
+                if len(linelist) < 3:
+                    sys.exit("Fault!")
+                elif len(linelist) > 3:
+                    output.write(l)
+                i += 1
+
+
+if __name__ == "__main__":
+    repair()
+                
+
