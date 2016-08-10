d9f118789d4da73c5c3f40cbb6bce946e0a65f4d
Joris Clement <7713214+joclement@users.noreply.github.com>
2016-08-10 02:30:12 +0200

    adds sample sh file for multiple runs

---
+++ programming/ga/src/sample_run.sh
@@ -0,0 +1,5 @@
+python3 start.py ../configfiles/garf_sed30.cfg --no-show
+python3 start.py ../configfiles/garf_sed30_SEL1_REP0.cfg --no-show
+python3 start.py ../configfiles/garf_sed30_SEL0_REP2.cfg --no-show
+python3 start.py ../configfiles/garf_sed30_SEL1_REP1.cfg --no-show
+python3 start.py ../configfiles/garf_sed30_SEL1_REP2.cfg --no-show
